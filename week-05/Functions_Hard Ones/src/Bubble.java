1734c792e40843e3b6d576faec9ba6d46df494fd
KecskesDora <dora.kecskes@gmail.com>
2019-11-18 23:34:46 +0100

    Bubble finished

---
+++ week-05/Functions_Hard Ones/src/Bubble.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class Bubble {
+    public static void main(String[] args) {
+        //  Create a function that takes a list of numbers as parameter
+        //  Returns a list where the elements are sorted in ascending numerical order
+        //  Make a second boolean parameter, if it's `true` sort that list descending
+
+        //  Example:
+        System.out.println(bubble(new int[] {34, 12, 24, 9, 5}));
+        //  should print [5, 9, 12, 24, 34]
+        advancedBubble(new int[] {34, 12, 24, 9, 5}, true);
+        //  should print [34, 24, 12, 9, 5]
+    }
+    private static List<Integer> bubble(int[] list) {
+        int temp = 0;
+        List<Integer> ascendingList = new ArrayList<Integer>();
+
+        for (int i = 0; i < list.length; i++) {
+            for (int j = i + 1; j < list.length; j++) {
+                if (list[i] > list[j]) {
+                    temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                }
+            }
+            ascendingList.add(i, list[i]);
+        }
+        return ascendingList;
+    }
+
+    private static List<Integer> advancedBubble(int[] list, boolean descending) {
+        int temp;
+        List<Integer> ascendingList = new ArrayList<Integer>();
+        List<Integer> descendingList = new ArrayList<Integer>();
+
+        if (descending == false) {
+            for (int i = 0; i < list.length; i++) {
+                for (int j = i + 1; j < list.length; j++) {
+                    if (list[i] > list[j]) {
+                        temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                    }
+                }
+                ascendingList.add(i, list[i]);
+            }
+            temp = 0;
+        } else if (descending == true) {
+            for (int i = 0; i < list.length; i++) {
+                for (int j = i + 1; j < list.length; j++) {
+                    if (list[i] < list[j]) {
+                        temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                    }
+                }
+                descendingList.add(i, list[i]);
+            }
+        }
+       return descendingList;
+    }
+}
---
+++ week-05/Functions_Hard Ones/src/Unique.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class Unique {
+    public static void main(String[] args) {
+        //  Create a function that takes a list of numbers as a parameter
+        //  Returns a list of numbers where every number in the list occurs only once
+
+        //  Example
+        System.out.println(unique(new int[]{1, 11, 34, 11, 52, 61, 1, 34}));
+        //  should print: `[1, 11, 34, 52, 61]`
+    }
+
+    private static List<Integer> unique(int[] list) {
+        Arrays.sort(list);
+        List<Integer> uniqueList = new ArrayList<Integer>();
+        int i = 0;
+        while (i < list.length) {
+            if (list[i] != list [i+1]) {
+                uniqueList.add(i, list[i]);
+
+            }
+            i += 2;
+        }
+        return uniqueList;
+    }
+}
+
+       /* for (int i = 0; i < list.length; i++) {
+            for (int j = i + 1; j < list.length; j++) {
+                if (list[i] == list[j]) {
+                    temp = list[i];
+                }
+            }
+            uniqueList.add(i, list[i]);
+        }
+        return uniqueList;*/
+
+    /*private static List<Integer> unique(int[] list) {
+        String[] stringList = new String[list.length];
+
+        for (int i = 0; i < list.length; i++) {
+            stringList[i] = String.valueOf(list[i]);
+            if Collections.
+        }
+
+    }*/
+    /*private static List<Integer> unique(int[] list) {
+        List<Integer> sortedList = new ArrayList<Integer>();
+        List<Integer> uniqueList = new ArrayList<Integer>();
+
+        for (int i = 0; i < list.length; i++) {
+            sortedList.add(i, list[i]);
+        }
+        Collections.sort(sortedList);
+
+        for (int i = 0; i < sortedList.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (sortedList.get(i) == sortedList.get(j)) {
+                    break;
+                }
+                if (i == j){
+                    uniqueList.add(i, sortedList.get(i));
+                }
+            }
+        }
+        return uniqueList;
+    }*/
+
+
+
