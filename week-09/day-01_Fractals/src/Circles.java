e933029635535ffb31477ed3dd010bd11827327b
KecskesDora <dora.kecskes@gmail.com>
2019-11-27 14:49:53 +0100

    Sierpinsky Carpet finished

--- week-09/day-01_Fractals/src/Circles.java
+++ week-09/day-01_Fractals/src/Circles.java
@@ -6,10 +6,11 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class Circles {
 
     public static void mainDraw(Graphics graphics) {
-        
+
         drawCircleFractal(0, 0, 600, 4, graphics);
 
     }
+
     public static void drawCircleFractal(int x, int y, int size, int level, Graphics graphics) {
         if (level == 0) return;
 
@@ -18,20 +19,20 @@ public class Circles {
         drawCircleFractal(x + size / 32, y + size / 8 * 3, size / 2, level - 1, graphics);
         drawCircleFractal(x + size / 2 - size / 32, y + size / 8 * 3, size / 2, level -1, graphics);
     }
-
-    public static void drawBiggerPattern(int x, int y, int size, Graphics graphics) {
+     // ez nem is kell:
+    /*public static void drawBiggerPattern(int x, int y, int size, Graphics graphics) {
         drawCirclePattern(x + size / 4, y, size / 2, graphics);
         drawCirclePattern(x + size / 32, y + size / 8 * 3, size / 2, graphics);
         drawCirclePattern(x + size / 2 - size / 32,y + size / 8 * 3, size / 2, graphics );
-    }
+    }*/
 
     public static void drawCirclePattern(int x, int y, int size, Graphics graphics) {
         //draw big circle
         graphics.drawOval(x, y, size, size);
-        //draw little circles
-        graphics.drawOval(x + size / 4, y, size / 2, size / 2);
-        graphics.drawOval(x + size / 32, y + size / 8 * 3,size / 2, size / 2);
-        graphics.drawOval(x + size / 2 - size / 32, y + size / 8 * 3, size / 2, size / 2);
+        //draw little circles, ez sem kell:
+        //graphics.drawOval(x + size / 4, y, size / 2, size / 2);
+        //graphics.drawOval(x + size / 32, y + size / 8 * 3,size / 2, size / 2);
+        //graphics.drawOval(x + size / 2 - size / 32, y + size / 8 * 3, size / 2, size / 2);
     }
 
     static int WIDTH = 600;
---
+++ week-09/day-02_Fractals/src/SierpinskyCarpet.java
@@ -0,0 +1,73 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class SierpinskyCarpet {
+
+    public static void mainDraw(Graphics graphics) {
+
+        drawSquareFractal(0, 0, WIDTH, 5, graphics);
+    }
+
+    public static void drawSquareFractal(int x, int y, int size, int level, Graphics graphics) {
+        if (level == 0) return;
+
+        drawSquarePattern(x, y, size, graphics);
+
+        drawSquareFractal(x, y, size / 3,level - 1, graphics);
+        drawSquareFractal(x + size / 3, y, size / 3, level - 1, graphics);
+        drawSquareFractal(x + size / 3 * 2, y, size / 3, level - 1, graphics);
+
+        drawSquareFractal(x , y + size / 3, size / 3, level - 1, graphics);
+        drawSquareFractal(x + size / 3 * 2, y + size / 3, size / 3, level - 1, graphics);
+
+        drawSquareFractal(x, y + size / 3 * 2, size / 3, level - 1, graphics);
+        drawSquareFractal(x + size / 3, y + size / 3 * 2, size / 3, level - 1, graphics);
+        drawSquareFractal(x + size / 3 * 2, y + size / 3 * 2, size / 3, level - 1, graphics);
+    }
+/*
+    public static void drawBiggerPattern(int x, int y, int size, Graphics graphics) {
+
+        drawSquarePattern(x, y, size, graphics);
+
+        drawSquarePattern(x, y, size / 3, graphics);
+        drawSquarePattern(x + size / 3, y, size / 3, graphics);
+        drawSquarePattern(x + size / 3 * 2, y, size / 3, graphics);
+
+        drawSquarePattern(x , y + size / 3, size / 3, graphics);
+        drawSquarePattern(x + size / 3 * 2, y + size / 3, size / 3, graphics);
+
+        drawSquarePattern(x, y + size / 3 * 2, size / 3, graphics);
+        drawSquarePattern(x + size / 3, y + size / 3 * 2, size / 3, graphics);
+        drawSquarePattern(x + size / 3 * 2, y + size / 3 * 2, size / 3, graphics);
+    }*/
+
+    public static void drawSquarePattern(int x, int y, int size, Graphics graphics) {
+
+        graphics.fillRect(x + size / 3, y + size / 3, size / 3, size / 3);
+
+    }
+
+    static int WIDTH = 600;
+    static int HEIGHT = 600;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
