4a180c14800b096f78a0c1c48a0493af3ad8f8eb
KecskesDora <dora.kecskes@gmail.com>
2019-12-16 21:58:27 +0100

    Fibonacci repaired

--- week-08/day-03_Recursion/src/Fibonacci.java
+++ week-08/day-03_Recursion/src/Fibonacci.java
@@ -6,13 +6,13 @@
 
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(fibonacci(5));
+        System.out.println(fibonacci(6));
     }
 
     public static int fibonacci(int n) {
         if (n == 1) {
             return 0;
-        } else if (n == 1 || n == 2) {
+        } else if (n == 2) {
             return 1;
         } else {
             return fibonacci(n - 1) + fibonacci(n - 2);
