cc71d405bf53dd3af47840b954ead243111508bc
KecskesDora <dora.kecskes@gmail.com>
2020-01-01 21:02:09 +0100

    Extension finished without working translate()

---
+++ week-11/Testing/Extension/src/Extension.java
@@ -0,0 +1,59 @@
+// Check out the folder. There's a work file and a test file.
+
+//  -  Run the tests, all 10 should be green (passing).
+//  -  The implementations though are not quite good.
+//  -  Create tests that'll fail, and will show how the implementations are wrong
+//      (You can assume that the implementation of join and split are good)
+//  -  After creating the tests, fix the implementations
+//  -  Check again, if you can create failing tests
+//  -  Implement if needed
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Extension {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int maxOfThree(int a, int b, int c) {
+        if (a > b && a > c)
+            return a;
+        else if (b > c)
+            return b;
+        else
+            return c;
+    }
+
+    float median(List<Integer> pool) {
+        Collections.sort(pool);
+        if (pool.size() % 2 == 1) {
+            return pool.get((pool.size() - 1) / 2);
+        } else {
+            return ((float) (pool.get(pool.size() / 2 - 1)) + pool.get(pool.size() / 2)) / 2;
+        }
+    }
+
+    boolean isVowel(char c) {
+        return Arrays.asList('a', 'A', 'á', 'Á', 'u', 'U', 'ú', 'Ú', 'ü', 'Ű', 'ö', 'Ö', "Ő", 'o', 'Ó', 'e', 'E', 'é', 'É', 'i', 'Í').contains(c);
+    }
+
+    String translate(String hungarian) {
+        //Still not working
+        int length = hungarian.length();
+        String translated = "";
+        for (int i = 0; i < length; i++) {
+
+            char c = hungarian.charAt(i);
+            if (isVowel(c)) {
+                translated = String.join(c + "v" + c, hungarian.split("" + c, length));
+                i += 3;
+                length += 2;
+            }
+
+        }
+        return translated;
+    }
+}
+
