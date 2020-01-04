a6edc6ef3448285482833554bf42f10853c93d81
KecskesDora <dora.kecskes@gmail.com>
2020-01-04 23:55:22 +0100

    ConnectTheDotsv2

--- week-06/day-01_Drawing/src/ConnectTheDotsv2.java
+++ week-06/day-01_Drawing/src/ConnectTheDotsv2.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class ConnectTheDots {
+public class ConnectTheDotsv2 {
     public static void mainDraw(Graphics graphics) {
         // Create a function that takes 2 parameters:
         // An array of {x, y} points and graphics
@@ -24,15 +27,21 @@ public class ConnectTheDots {
                 {80, 90}, {90, 90}, {100, 70},
                 {120, 100}, {85, 130}, {50, 100}};
 
-        connectLines(polygon1, graphics);
-        connectLines(polygon2, graphics);
-    }
-    public static void connectLines(int points[][], Graphics graphics) {
-        for (int i = 0; i < points.length - 1; i++) {
-            graphics.drawLine(points[i][0], points[i][1], points[i + 1][0], points[i + 1][1]);
+        int xPoints[] = {polygon1[0][0], polygon1[1][0], polygon1[2][0], polygon1[3][0]};
+        int yPoints[] = {polygon1[0][1], polygon1[1][1], polygon1[2][1], polygon1[3][1]};
+        graphics.drawPolygon(xPoints, yPoints, polygon1.length);
+
+    //ez nem jó még:
+        for (int i = 0; i < polygon2.length; i++) {
+            List<Integer> x2Points = new ArrayList<>();
+            x2Points.add(i, polygon2[i][0]);
+            List<Integer> y2Points = new ArrayList<>();
+            y2Points.add(i, polygon2[i][1]);
+            System.out.println(y2Points);
+            //graphics.drawPolygon(x2Points, y2Points, polygon2.length);
+
         }
-        graphics.drawLine(points[points.length - 1][0], points[points.length - 1][1], points[0][0], points[0][1]);
-    }
+        }
 
     // Don't touch the code below
     static int WIDTH = 320;
