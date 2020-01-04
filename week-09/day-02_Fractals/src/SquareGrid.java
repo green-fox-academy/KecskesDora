004f24391d1b293009d675e09f361b3ff27d1aeb
KecskesDora <dora.kecskes@gmail.com>
2020-01-04 23:53:21 +0100

    made colorful for presentation

--- week-09/day-02_Fractals/src/SquareGrid.java
+++ week-09/day-02_Fractals/src/SquareGrid.java
@@ -1,21 +1,21 @@
 import com.sun.corba.se.impl.orbutil.graph.Graph;
-
 import javax.swing.*;
 import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class SquareGrid {
-        public static void mainDraw(Graphics graphics) {
+    public static void mainDraw(Graphics graphics) {
+
         Graphics2D g2= (Graphics2D) graphics;
         drawSquareFractal(0, 0, WIDTH, 4, g2);
     }
 
-
     public static void drawSquareFractal(int x, int y, int size, int level, Graphics2D graphics) {
         if (level == 0) return;
-                
-        graphics.setStroke(new BasicStroke((int) (Math.pow((double)level, 2.0))));
+
+        graphics.setStroke(new BasicStroke((float) Math.pow(level, 2)));
+
         drawSquare(x, y, size, graphics);
 
         drawSquareFractal(x , y, size / 2, level - 1, graphics);
@@ -24,11 +24,21 @@ public class SquareGrid {
         drawSquareFractal(x + size / 2, y + size / 2, size / 2, level - 1, graphics);
     }
 
+        public static void drawBiggerPattern(int x, int y, int size, Graphics graphics) {
+
+        drawSquare(x, y, size, graphics);
+
+        drawSquare(x , y, size / 2, graphics);
+        drawSquare(x + size / 2, y, size / 2, graphics);
+        drawSquare(x, y + size / 2, size / 2, graphics);
+        drawSquare(x + size / 2, y + size / 2, size / 2, graphics);
+    }
+
     public static void drawSquare(int x, int y, int size, Graphics graphics) {
 
-        Color color = new Color((int)(Math.random() * 255), (int)(Math.random() * 255), (int)(Math.random() * 255));
-        graphics.setColor(color);
-        graphics.drawRect(x + size / 4, y + size / 4, size / 2, size / 2);
+        //Color color = new Color((int)(Math.random() * 256), (int)(Math.random() * 256), (int)(Math.random() * 256));
+        //graphics.setColor(color);
+        graphics.drawRect(0 + size / 4, 0 + size / 4, size / 2, size / 2);
     }
 
     static int WIDTH = 600;
