142a48ec69cc16a86186b4eeee0e350ae29d362b
KecskesDora <dora.kecskes@gmail.com>
2020-02-11 10:43:27 +0100

    add(Hello World Web App)

--- week-17/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-17/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,14 +1,21 @@
 package com.greenfoxacademy.springstart.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class HelloRESTController {
 
-    @RequestMapping(value="/greeting", method= RequestMethod.GET)
+    /*@RequestMapping(value="/greeting", method= RequestMethod.GET)
     public Greeting greeting() {
         return new Greeting(1, "Hello, World");
+    }*/
+
+    private AtomicLong numberOfCalls = new AtomicLong();
+
+    @GetMapping(value = "/greeting")
+    public Greeting greeting(@RequestParam String name){
+        return new Greeting(numberOfCalls.incrementAndGet(), "Hello, " + name + "!");
     }
 }
