83216404e7827804ca8f3125aafa259343d3df63
KecskesDora <dora.kecskes@gmail.com>
2020-02-13 15:00:29 +0100

    add(thymeleaf start ex)

---
+++ week-17/thymeleafstart/src/test/java/com/greenfoxacademy/thymeleafstart/ThymeleafstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.thymeleafstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
