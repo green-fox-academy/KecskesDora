f8955008918bb2c91e0ca63e51b28094ed59e31a
KecskesDora <dora.kecskes@gmail.com>
2020-02-14 14:59:06 +0100

    started Bank Of Symba

---
+++ week-17/BankOfSymba/src/main/java/com/greenfoxacademy/bankofsymba/controllers/BankController.java
@@ -0,0 +1,57 @@
+package com.greenfoxacademy.bankofsymba.controllers;
+
+import com.greenfoxacademy.bankofsymba.models.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class BankController {
+
+    private List<BankAccount> bankAccounts = new ArrayList<>();
+
+    public BankController() {
+        bankAccounts.add(new BankAccount("Simba", 2000, "lion"));
+        bankAccounts.add(new BankAccount("Timon", 1000, "suricata"));
+        bankAccounts.add(new BankAccount("Pumbaa", 1100, "warthog"));
+        bankAccounts.add(new BankAccount("Zordon", 1500, "lion"));
+        bankAccounts.add(new BankAccount("Ed", 800, "hyena"));
+        bankAccounts.add(new BankAccount("Banzai", 900, "hyena"));
+    }
+
+    //@RequestMapping(path = "/show", method = RequestMethod.GET)
+    @GetMapping(value = "/show")
+    public String showAccount(Model model) {
+        model.addAttribute("account", bankAccounts.get(0));
+        return "index";
+    }
+
+    //@RequestMapping(path = "/enjoy", method = RequestMethod.GET)
+    @GetMapping(value = "/enjoy")
+    public String enjoyYourself(Model model) {
+        model.addAttribute("text", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
+        return "utext-example";
+    }
+
+    @GetMapping(value = "/enjoy2")
+    @ResponseBody
+    public String enjoyYourself() {
+        return "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
+    }
+
+    @GetMapping("/accounts")
+    public String listAccounts(Model model) {
+        model.addAttribute("accounts", bankAccounts);
+        return "table";
+    }
+
+
+}
+
---
+++ week-17/BankOfSymba/src/test/java/com/greenfoxacademy/bankofsymba/BankofsymbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.bankofsymba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankofsymbaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
