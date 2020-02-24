e89c55c3dda14ce2b939d22dbf070ff758ecefca
KecskesDora <dora.kecskes@gmail.com>
2020-02-24 11:27:30 +0100

    add(login page)

--- week-19/ProgrammerFoxClub/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/MainController.java
+++ week-19/ProgrammerFoxClub/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/MainController.java
@@ -1,13 +1,27 @@
 package com.greenfoxacademy.programmerfoxclub.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String getMainPage() {
+    public String getMainPage(Model model, @RequestParam(value = "name", required = true) String name) {
+        model. addAttribute("name", name);
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(String name) {
+        return "redirect:/?name=" + name;
     }
 }
---
+++ week-19/ProgrammerFoxClub/src/main/java/com/greenfoxacademy/programmerfoxclub/models/Fox.java
@@ -0,0 +1,10 @@
+package com.greenfoxacademy.programmerfoxclub.models;
+
+public class Fox {
+    private String name;
+
+    public Fox(String name) {
+        this.name = name;
+    }
+
+}
