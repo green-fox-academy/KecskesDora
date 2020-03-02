e73fe671b74afd5e1b811e5b3df048fb336015f7
KecskesDora <dora.kecskes@gmail.com>
2020-03-02 11:36:53 +0100

    add(TodoRepository)

--- week-20/DatabaseIntegration/ListingTodosH2/src/main/java/com/greenfoxacademy/listingtodos/controllers/TodoController.java
+++ week-20/DatabaseIntegration/ListingTodosH2/src/main/java/com/greenfoxacademy/listingtodos/controllers/TodoController.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.listingtodos.controllers;
 
 import com.greenfoxacademy.listingtodos.models.Todo;
+import com.greenfoxacademy.listingtodos.repositories.TodoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/todo")
 public class TodoController {
 
-    public TodoController() {
+    TodoRepository todoRepository;
 
+    public TodoController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
     }
 
     @RequestMapping(value = {"/", "/list"}, method = RequestMethod.GET)
