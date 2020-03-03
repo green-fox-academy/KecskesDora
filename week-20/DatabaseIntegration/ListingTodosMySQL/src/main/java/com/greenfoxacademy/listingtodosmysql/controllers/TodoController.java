afa12db7dd1ed2e4713e360d71b28b42b15ac86a
KecskesDora <dora.kecskes@gmail.com>
2020-03-03 22:26:07 +0100

    add(creating function)

--- week-20/DatabaseIntegration/ListingTodosMySQL/src/main/java/com/greenfoxacademy/listingtodosmysql/controllers/TodoController.java
+++ week-20/DatabaseIntegration/ListingTodosMySQL/src/main/java/com/greenfoxacademy/listingtodosmysql/controllers/TodoController.java
@@ -1,9 +1,11 @@
 package com.greenfoxacademy.listingtodosmysql.controllers;
 
+import com.greenfoxacademy.listingtodosmysql.models.Todo;
 import com.greenfoxacademy.listingtodosmysql.repositories.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,11 +31,22 @@ public class TodoController {
     public String list(Model model, @RequestParam(value = "isActive", required = false) String isActive) {
         if (isActive == null) {
             model.addAttribute("todos", todoRepository.findAll());
-
         } else {
-            model.addAttribute("todos", todoRepository.findAllByIsDone(isActive.equals("true")));
+            model.addAttribute("todos", todoRepository.findAllByIsDone(!Boolean.parseBoolean(isActive)));
         }
         return "todolist";
+    }
+
+    @RequestMapping(value = {"/add"}, method = RequestMethod.GET)
+    public String renderAddForm(Model model, @ModelAttribute("todo") Todo todo) {
+        model.addAttribute("todo", todo);
+        return "add";
+    }
+
+    @RequestMapping(value = {"/add"}, method = RequestMethod.POST)
+    public String addNewTodo(@ModelAttribute("todo") Todo todo) {
+        todoRepository.save(todo);
+        return "redirect:/todos/list";
     }
 }
 
