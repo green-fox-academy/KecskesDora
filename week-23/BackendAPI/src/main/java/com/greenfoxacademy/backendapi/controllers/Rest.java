c425a445cd453bf9ca550f0b8c5d09d96babc3a7
KecskesDora <dora.kecskes@gmail.com>
2020-03-27 21:26:01 +0100

    add(Greeter exercise)

--- week-23/BackendAPI/src/main/java/com/greenfoxacademy/backendapi/controllers/Rest.java
+++ week-23/BackendAPI/src/main/java/com/greenfoxacademy/backendapi/controllers/Rest.java
@@ -2,6 +2,7 @@ package com.greenfoxacademy.backendapi.controllers;
 
 import com.greenfoxacademy.backendapi.models.dtos.ErrorMessage;
 import com.greenfoxacademy.backendapi.models.entities.Doubling;
+import com.greenfoxacademy.backendapi.models.entities.Greeter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,9 +20,17 @@ public class Rest {
         }
     }
 
-    @GetMapping("/doubling")
+    @GetMapping("/greeter")
     public ResponseEntity greeter(@RequestParam (required = false) String name, @RequestParam (required = false) String title) {
-
+        if (name == null && title == null) {
+            return ResponseEntity.status(400).body(new ErrorMessage("Please provide a name and a title!"));
+        } else if (name != null && title == null) {
+            return ResponseEntity.status(400).body(new ErrorMessage("Please provide a title!"));
+        } else if (name == null && title != null) {
+            return ResponseEntity.status(400).body(new ErrorMessage("Please provide a name!"));
+        } else {
+            return ResponseEntity.status(200).body(new Greeter(name, title));
+        }
     }
 }
 
--- week-23/BackendAPI/src/main/java/com/greenfoxacademy/backendapi/models/entities/Greeter.java
+++ week-23/BackendAPI/src/main/java/com/greenfoxacademy/backendapi/models/entities/Greeter.java
@@ -1,5 +1,20 @@
 package com.greenfoxacademy.backendapi.models.entities;
 
-public class Greeter {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class Greeter {
+    @JsonProperty("welcome_message")
+    private String welcomeMessage;
+
+    public Greeter(String name, String title) {
+        this.welcomeMessage = "Oh, hi there " + name + ", my dear " + title + "!";
+    }
+
+    public String getWelcomeMessage() {
+        return welcomeMessage;
+    }
+
+    public void setWelcomeMessage(String welcomeMessage) {
+        this.welcomeMessage = welcomeMessage;
+    }
 }
